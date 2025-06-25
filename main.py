5f7b067f8f45e076146076bf396708d32e92a36c
Usama Afzal <usamafzal36@gmail.com>
2025-06-25 20:30:53 +0500

    Model Trainer

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from src.components.data_ingestion import DataIngestion, DataIngestionConfig
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 from src.exception import CustomException
 import sys
+
 
 if __name__=="__main__":
     try:
@@ -11,5 +13,10 @@ if __name__=="__main__":
         data_transformation = DataTransformation()
         train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
 
+        model_trainer = ModelTrainer()
+        results = model_trainer.initiate_model_trainer(train_arr, test_arr)
+        print(results)
+
+        
     except Exception as e:
         raise CustomException(e, sys)

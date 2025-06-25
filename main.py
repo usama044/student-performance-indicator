0bc3c80cc3a0323969b7fa12cb765085c9667612
Usama Afzal <usamafzal36@gmail.com>
2025-06-25 18:10:07 +0500

    Data Transformation

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from src.components.data_ingestion import DataIngestion, DataIngestionConfig
-
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.exception import CustomException
+import sys
 
 if __name__=="__main__":
-    data_ingestion = DataIngestion()
-    train_data, test_data = data_ingestion.initiate_data_ingestion()
-    
+    try:
+        data_ingestion = DataIngestion()
+        train_data, test_data = data_ingestion.initiate_data_ingestion()
+
+        data_transformation = DataTransformation()
+        train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    except Exception as e:
+        raise CustomException(e, sys)
